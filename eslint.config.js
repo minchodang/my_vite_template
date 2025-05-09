5b302b97396bbdd1667f582331f86bfedd755e22
minchodang <minsu910725@gmail.com>
2025-05-09 21:25:25 +0900

    fix: eslint 수정 및 기타 처리

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,49 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import { parser } from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'lib', 'idea'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist', 'node_modules', 'lib'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: parser,
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
-      '@typescript-eslint': eslintPlugin,
-      react: react,
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'jsx-a11y': jsxA11y,
-      import: importPlugin,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...eslintPlugin.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
-      quotes: ['error', 'single'],
-      'no-unused-vars': 'off',
-
+      ...tseslint.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      indent: [
-        'error',
-        'tab',
-        {
-          SwitchCase: 1,
-          ignoredNodes: ['TemplateLiteral'],
-        },
-      ],
-      'react/jsx-indent': ['error', 'tab'],
-      'react/jsx-indent-props': ['error', 'tab'],
+      'prettier/prettier': 'error',
     },
-  }
-);
+  },
+];
