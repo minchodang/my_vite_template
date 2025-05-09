0c44f826ddbd00749f6af0c6c4d7dd74594ede46
minchodang <minsu910725@gmail.com>
2025-05-09 21:37:22 +0900

    feat: eslint 접근성 lint 추가

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
   {
@@ -32,12 +33,14 @@ export default [
       'react-refresh': reactRefresh,
       '@typescript-eslint': tseslint,
       prettier: prettierPlugin,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
       'react/jsx-no-target-blank': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
